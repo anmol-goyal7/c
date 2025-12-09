1d17fa0892aee393a4365ef7522cc5ce5bc80927
anmol <anmolafk7@gmail.com>
2025-12-09 18:35:48 +0530

    Optimized matrix multiplication with OpenMP

--- matrix_mult.c
+++ matrix_mult.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<omp.h>
 
 int main()
 {
@@ -30,7 +31,8 @@ int main()
                 result[i][j] = 0;
             }
         }
-
+	
+	#pragma omp parallel for private(j, k)
         for (i = 0; i < 3; i++) {
             for (j = 0; j < 3; j++) {
                 for (k = 0; k < 3; k++) {
